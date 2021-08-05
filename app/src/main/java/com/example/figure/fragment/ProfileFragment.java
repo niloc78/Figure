e2b7c0bb938f082499496fa114fb526672f1f5ad
colin <colin.ruan@stonybrook.edu>
2021-08-05 00:05:11 -0400

    -working on backend/login
    -needs to remove delivery
    -began on profile
    - need changes to logo/logo color

--- app/src/main/java/com/example/figure/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/figure/fragment/ProfileFragment.java
@@ -1,20 +1,67 @@
 package com.example.figure.fragment;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
+
+import com.example.figure.view.NutritionDialog;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.mikhaellopez.circularimageview.*;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
 import androidx.fragment.app.Fragment;
 
 import com.example.figure.MainActivity;
 import com.example.figure.R;
+import com.google.android.material.imageview.ShapeableImageView;
 
-public class ProfileFragment extends Fragment {
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
+public class ProfileFragment extends Fragment implements NutritionDialog.SetListener {
     Context context;
     View _rootView;
+    CircularImageView circularImageView;
+    TextView name;
+    TextView heightView;
+    TextView weightView;
+    TextView goalWeightView;
+    ImageButton editButton;
+    CircularProgressIndicator calProgressBar;
+    CircularProgressIndicator fatProgressBar;
+    CircularProgressIndicator carbProgressBar;
+    CircularProgressIndicator proteinProgressBar;
+    TextView calories;
+    TextView fat;
+    TextView carbs;
+    TextView protein;
+    TextView goalCalories;
+    TextView goalFat;
+    TextView goalCarbs;
+    TextView goalProtein;
+
     //    public IngredientFragment() {
 //        super(R.layout.ingred_frag_layout);
 //    }
@@ -35,7 +82,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         if (_rootView == null) {
 
-            _rootView = inflater.inflate(R.layout.user_start, container, false);
+            _rootView = inflater.inflate(R.layout.profile_layout, container, false);
 
         }
         return _rootView;
@@ -44,6 +91,18 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) { // what happens after onCreateView
         super.onViewCreated(view, savedInstanceState);
+        if (circularImageView == null) {
+            initViews(view);
+            //setGoalCalories(2300);
+//            calProgressBar.setOnClickListener(v -> {
+//
+//                Random rand = new Random();
+//                int cal = rand.nextInt(2300);
+//                setCalories(cal);
+//                //setGoalCalories(cal);
+//            });
+        }
+        //setProfilePic(view);
     }
 
     @Override
@@ -55,5 +114,258 @@ public class ProfileFragment extends Fragment {
     @Override
     public String toString () {
         return "profileFragment";
+    }
+
+
+    void initViews(View view) {
+        circularImageView = (CircularImageView) view.findViewById(R.id.profile_image);
+        name = (TextView) view.findViewById(R.id.profile_name);
+        heightView = (TextView) view.findViewById(R.id.profile_height);
+        weightView = (TextView) view.findViewById(R.id.profile_weight);
+        goalWeightView = (TextView) view.findViewById(R.id.profile_goal_weight);
+        editButton = (ImageButton) view.findViewById(R.id.edit_profile_button);
+        calProgressBar = (CircularProgressIndicator) view.findViewById(R.id.calories_progress_bar);
+        fatProgressBar = (CircularProgressIndicator) view.findViewById(R.id.fat_progress_bar);
+        carbProgressBar = (CircularProgressIndicator) view.findViewById(R.id.carbs_progress_bar);
+        proteinProgressBar = (CircularProgressIndicator) view.findViewById(R.id.protein_progress_bar);
+        calories = (TextView) view.findViewById(R.id.calories);
+        fat = (TextView) view.findViewById(R.id.fat);
+        carbs = (TextView) view.findViewById(R.id.carbs);
+        protein = (TextView) view.findViewById(R.id.protein);
+        goalCalories = (TextView) view.findViewById(R.id.goal_calories);
+        goalFat = (TextView) view.findViewById(R.id.goal_fat);
+        goalCarbs = (TextView) view.findViewById(R.id.goal_carbs);
+        goalProtein = (TextView) view.findViewById(R.id.goal_protein);
+
+        calProgressBar.setOnClickListener(v -> {
+            openDialog(0);
+        });
+        fatProgressBar.setOnClickListener(v -> {
+            openDialog(1);
+        });
+        carbProgressBar.setOnClickListener(v -> {
+            openDialog(2);
+        });
+        proteinProgressBar.setOnClickListener(v -> {
+            openDialog(3);
+        });
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.omelette);
+        circularImageView.setImageBitmap(bitmap);
+
+
+    }
+    void setProfilePic(Bitmap bitmap) {
+        circularImageView.setImageBitmap(bitmap);
+//        ShapeableImageView profileImageView = (ShapeableImageView) view.findViewById(R.id.profile_image);
+//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.omelette);
+//        profileImageView.setImageBitmap(resizeProfileImage(bitmap));
+    }
+    public Bitmap getProfilePic() {
+        return ((BitmapDrawable)circularImageView.getDrawable()).getBitmap();
+    }
+
+    private Bitmap resizeProfileImage(Bitmap bitmap) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+
+        int minwidth = dpToPx(200, context);
+        int minheight = dpToPx(200, context);
+        float scaleWidth = ((float) minwidth)/width;
+        float scaleHeight = ((float) minheight)/height;
+
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        Bitmap resizedBitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, false);
+        bitmap.recycle();
+        Bitmap crop = getCircleCrop(resizedBitmap);
+        return crop;
+
+    }
+    private Bitmap getCircleCrop(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0,0,bitmap.getWidth(), bitmap.getHeight());
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0,0,0,0);
+        paint.setColor(color);
+        canvas.drawCircle(bitmap.getWidth()/2, bitmap.getHeight()/2, bitmap.getWidth()/2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+    void setHeight(int feet, int inches) {
+        heightView.setText(feet + "'" + inches);
+    }
+    void setWeight(int pounds) {
+        weightView.setText(pounds);
+    }
+    void setGoalWeight(int pounds) {
+        goalWeightView.setText(pounds);
+    }
+    void setName(String name) {
+        this.name.setText(name);
+    }
+    void setProfilePic(Bitmap bitmap) {
+        circularImageView.setImageBitmap(bitmap);
+    }
+
+    void setGoalCalories(int goalCalories) {
+        ValueAnimator animator = ValueAnimator.ofInt(Integer.parseInt(this.goalCalories.getText().toString()), goalCalories);
+        animator.setDuration(700);
+        animator.addUpdateListener(animation -> {
+            this.goalCalories.setText(animation.getAnimatedValue().toString());
+        });
+        animator.start();
+        //this.goalCalories.setText(Integer.toString(goalCalories));
+        this.calProgressBar.setMax(goalCalories);
+    }
+    public void setCalories(int cal) {
+        ValueAnimator animator = ValueAnimator.ofInt(Integer.parseInt(calories.getText().toString()), cal);
+        animator.setDuration(700);
+        animator.addUpdateListener(animation -> {
+            calories.setText(animation.getAnimatedValue().toString());
+        });
+        animator.start();
+        //calories.setText("" + cal);
+        //calProgressBar.setProgress(cal);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            calProgressBar.setProgress(cal, true);
+        } else {
+            ObjectAnimator o = ObjectAnimator.ofInt(calProgressBar, "progress", cal);
+            o.setInterpolator(new AccelerateDecelerateInterpolator());
+            o.setDuration(700).start();
+            //ObjectAnimator.ofInt(calProgressBar, "progress", cal).setDuration(300).setInterpolator(new AccelerateDecelerateInterpolator()).start();
+        }
+    }
+    void setGoalFat(int goalFat) {
+        ValueAnimator animator = ValueAnimator.ofInt(Integer.parseInt(this.goalFat.getText().toString()), goalFat);
+        animator.setDuration(700);
+        animator.addUpdateListener(animation -> {
+            this.goalFat.setText(animation.getAnimatedValue().toString());
+        });
+        animator.start();
+
+        //this.goalFat.setText("" + goalFat);
+        this.fatProgressBar.setMax(goalFat);
+    }
+    void setGoalCarbs(int goalCarbs) {
+        ValueAnimator animator = ValueAnimator.ofInt(Integer.parseInt(this.goalCarbs.getText().toString()), goalCarbs);
+        animator.setDuration(700);
+        animator.addUpdateListener(animation -> {
+            this.goalCarbs.setText(animation.getAnimatedValue().toString());
+        });
+        animator.start();
+        //this.goalCarbs.setText("" + goalCarbs);
+        this.carbProgressBar.setMax(goalCarbs);
+    }
+    void setGoalProtein(int goalProtein) {
+        ValueAnimator animator = ValueAnimator.ofInt(Integer.parseInt(this.goalProtein.getText().toString()), goalProtein);
+        animator.setDuration(700);
+        animator.addUpdateListener(animation -> {
+            this.goalProtein.setText(animation.getAnimatedValue().toString());
+        });
+        animator.start();
+        //this.goalProtein.setText("" + goalProtein);
+        this.proteinProgressBar.setMax(goalProtein);
+    }
+
+    void setFat(int fat) {
+        ValueAnimator animator = ValueAnimator.ofInt(Integer.parseInt(this.fat.getText().toString()), fat);
+        animator.setDuration(700);
+        animator.addUpdateListener(animation -> {
+            this.fat.setText(animation.getAnimatedValue().toString());
+        });
+        animator.start();
+        //this.fat.setText("" + fat);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            this.fatProgressBar.setProgress(fat, true);
+        } else {
+            ObjectAnimator.ofInt(this.fatProgressBar, "progress", fat).setDuration(300).start();
+        }
+    }
+    void setCarbs(int carbs) {
+        ValueAnimator animator = ValueAnimator.ofInt(Integer.parseInt(this.carbs.getText().toString()), carbs);
+        animator.setDuration(700);
+        animator.addUpdateListener(animation -> {
+            this.carbs.setText(animation.getAnimatedValue().toString());
+        });
+        animator.start();
+        //this.carbs.setText("" + carbs);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            this.carbProgressBar.setProgress(carbs, true);
+        } else {
+            ObjectAnimator.ofInt(this.carbProgressBar, "progress", carbs).setDuration(300).start();
+        }
+    }
+    void setProtein(int protein) {
+        ValueAnimator animator = ValueAnimator.ofInt(Integer.parseInt(this.protein.getText().toString()), protein);
+        animator.setDuration(700);
+        animator.addUpdateListener(animation -> {
+            this.protein.setText(animation.getAnimatedValue().toString());
+        });
+        animator.start();
+        //this.protein.setText("" + protein);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            this.proteinProgressBar.setProgress(protein, true);
+        } else {
+            ObjectAnimator.ofInt(this.proteinProgressBar, "progress", protein).setDuration(300).start();
+        }
+    }
+
+    public String getCalories() {
+        return calories.getText().toString();
+    }
+    public String getFat() {
+        return fat.getText().toString();
+    }
+    public String getCarbs() {
+        return carbs.getText().toString();
+    }
+    public String getProtein() {
+        return protein.getText().toString();
+    }
+    public String getGoalCalories() {
+        return goalCalories.getText().toString();
+    }
+    public String getGoalFat() {
+        return goalFat.getText().toString();
+    }
+    public String getGoalCarbs() {
+        return goalCarbs.getText().toString();
+    }
+    public String getGoalProtein() {
+        return goalProtein.getText().toString();
+    }
+
+    void openDialog(int i) {
+        NutritionDialog dialog = new NutritionDialog(i);
+        dialog.show(getChildFragmentManager(), "nutrition dialog");
+    }
+
+
+    private static int dpToPx(float dp, Context context) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+    }
+
+    @Override
+    public void setValues(String currVal, String goalVal, String mode) {
+        if (mode.equalsIgnoreCase("calories")) {
+            setGoalCalories(Integer.parseInt(goalVal));
+            setCalories(Integer.parseInt(currVal));
+        } else if (mode.equalsIgnoreCase("fat")) {
+            setGoalFat(Integer.parseInt(goalVal));
+            setFat(Integer.parseInt(currVal));
+        } else if (mode.equalsIgnoreCase("carbs")) {
+            setGoalCarbs(Integer.parseInt(goalVal));
+            setCarbs(Integer.parseInt(currVal));
+        } else if (mode.equalsIgnoreCase("protein")) {
+            setGoalProtein(Integer.parseInt(goalVal));
+            setProtein(Integer.parseInt(currVal));
+        }
     }
 }
---
+++ app/src/main/java/com/example/figure/view/NutritionDialog.java
@@ -0,0 +1,112 @@
+package com.example.figure.view;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.core.content.ContextCompat;
+
+import com.example.figure.AddIngredientDialog;
+import com.example.figure.R;
+import com.example.figure.fragment.ProfileFragment;
+
+public class NutritionDialog extends AppCompatDialogFragment {
+
+    private EditText edit_Curr;
+    private EditText edit_Goal;
+    private NutritionDialog.SetListener listener;
+    private String mode;
+
+
+
+    public NutritionDialog(int i) {
+        this.mode = i == 0 ? "Calories" : i == 1 ? "Fat" : i == 2 ? "Carbs" : i == 3 ? "Protein" : "";
+    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.set_goals_dialog, null);
+
+        AlertDialog dialog = builder.setView(view)
+                .setNegativeButton("Cancel", (dialogInterface, i) -> {
+
+                })
+                .setPositiveButton("Set", (dialog1, which) -> {
+                    String currVal = edit_Curr.getText().toString();
+                    String goalVal = edit_Goal.getText().toString();
+
+                    if (currVal.replace(" ", "").equalsIgnoreCase("")) {
+                        currVal = "0";
+                    }
+                    if (goalVal.replace(" ", "").equalsIgnoreCase("")) {
+                        goalVal = "0";
+                    }
+
+                    listener.setValues(currVal, goalVal, mode);
+
+//                        if (ingred.replace(" ", "").equalsIgnoreCase("")) {
+//                            Toast.makeText(getContext(), "Cannot have blank ingredient", Toast.LENGTH_SHORT).show();
+//                        } else {
+//                            listener.addIngredient(ingred);
+//                        }
+                }).show();
+
+        edit_Curr = view.findViewById(R.id.edit_curr);
+        edit_Goal = view.findViewById(R.id.edit_goal);
+        ProfileFragment parFrag = (ProfileFragment) getParentFragment();
+        if (mode.equalsIgnoreCase("calories")) {
+            edit_Curr.setTextColor(0xFFB20000);
+            edit_Curr.setText(parFrag.getCalories());
+            edit_Goal.setText(parFrag.getGoalCalories());
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(0xFFB20000);
+        } else if (mode.equalsIgnoreCase("fat")) {
+            edit_Curr.setTextColor(0xFFF9CC92);
+            edit_Curr.setText(parFrag.getFat());
+            edit_Goal.setText(parFrag.getGoalFat());
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(0xFFF9CC92);
+        } else if (mode.equalsIgnoreCase("carbs")) {
+            edit_Curr.setTextColor(0xFFCFFFDD);
+            edit_Curr.setText(parFrag.getCarbs());
+            edit_Goal.setText(parFrag.getGoalCarbs());
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(0xFFCFFFDD);
+        } else if (mode.equalsIgnoreCase("protein")) {
+            edit_Curr.setTextColor(0xFFCFE1FF);
+            edit_Curr.setText(parFrag.getProtein());
+            edit_Goal.setText(parFrag.getGoalProtein());
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(0xFFCFE1FF);
+        }
+
+
+        dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setBackground(ContextCompat.getDrawable(getContext(), R.color.white));
+        dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(ContextCompat.getColor(getContext(), R.color.black));
+
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setBackground(ContextCompat.getDrawable(getContext(), R.color.white));
+
+
+
+        return dialog;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            listener = (NutritionDialog.SetListener) getParentFragment();
+        } catch (ClassCastException e) {
+            throw new ClassCastException("Calling Fragment must implement NutritionDialog");
+        }
+    }
+
+    public interface SetListener {
+        void setValues(String currVal, String goalVal, String mode);
+    }
+}
+
